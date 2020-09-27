e7d89bf4400af0b86d089d6985342e512c627585
charcoJP <charco2356@gmail.com>
2020-09-27 23:32:38 +0900

    Jetpack Room を導入

---
+++ app/src/main/java/jp/co/charco/praisesreminder/data/db/AppDatabase.kt
@@ -0,0 +1,8 @@
+package jp.co.charco.praisesreminder.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+}
