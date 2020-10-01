799bce3a82cc0e372cf7160dafc37f5474b2d0ca
charcoJP <charco2356@gmail.com>
2020-10-01 21:26:01 +0900

    converter 配下に移動してリネーム

--- app/src/main/java/jp/co/charco/praisesreminder/data/db/AppDatabase.kt
+++ app/src/main/java/jp/co/charco/praisesreminder/data/db/AppDatabase.kt
@@ -3,11 +3,12 @@ package jp.co.charco.praisesreminder.data.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import jp.co.charco.praisesreminder.data.db.converter.LocalDateTimeConverter
 import jp.co.charco.praisesreminder.data.db.converter.LocalDateConverter
 import jp.co.charco.praisesreminder.data.db.entity.Praise
 
 @Database(entities = [Praise::class], version = 1)
-@TypeConverters(value = [Converters::class, LocalDateConverter::class])
+@TypeConverters(value = [LocalDateTimeConverter::class, LocalDateConverter::class])
 abstract class AppDatabase : RoomDatabase() {
     abstract fun praiseDao(): PraiseDao
 }
