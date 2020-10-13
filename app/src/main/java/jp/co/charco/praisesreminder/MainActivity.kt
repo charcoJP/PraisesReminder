04bbc58a584c5399c949f86af4c01b5ecb0b1270
charcoJP <charco2356@gmail.com>
2020-10-13 23:53:52 +0900

    カレンダー選択時にページを切り替える様にした

--- app/src/main/java/jp/co/charco/praisesreminder/MainActivity.kt
+++ app/src/main/java/jp/co/charco/praisesreminder/MainActivity.kt
@@ -78,6 +78,11 @@ class MainActivity : AppCompatActivity(), OnInputSubmitListener {
         viewModel.showInputBottomSheet.observe(this, EventObserver {
             showInputBottomSheet(it)
         })
+
+        viewModel.changePageEvent.observe(this, EventObserver {
+            val currentPosition = binding.viewPager.currentItem
+            binding.viewPager.setCurrentItem(currentPosition + it, true)
+        })
     }
 
     override fun onSubmit(praise: Praise) {
