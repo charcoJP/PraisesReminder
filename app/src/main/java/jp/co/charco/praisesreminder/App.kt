0958bed527594b03a756e9f091bb9ebbbf527e03
charcoJP <charco2356@gmail.com>
2020-10-25 01:03:30 +0900

    :bug: 起動するたびにアラームを設定する+19時以降に起動すると通知が出るバグ対応

--- app/src/main/java/jp/co/charco/praisesreminder/App.kt
+++ app/src/main/java/jp/co/charco/praisesreminder/App.kt
@@ -2,19 +2,6 @@ package jp.co.charco.praisesreminder
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import jp.co.charco.praisesreminder.util.AlarmHelper
-import java.util.*
 
 @HiltAndroidApp
-class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
-
-        // TODO: 設定時刻を可変にしたい
-        AlarmHelper.createDailyAlarm(this, 0, Calendar.getInstance().apply {
-            timeInMillis = System.currentTimeMillis()
-            set(Calendar.HOUR_OF_DAY, 19)
-            set(Calendar.MINUTE, 0)
-        })
-    }
-}
+class App : Application()
--- app/src/main/java/jp/co/charco/praisesreminder/MainViewModel.kt
+++ app/src/main/java/jp/co/charco/praisesreminder/MainViewModel.kt
@@ -1,13 +1,18 @@
 package jp.co.charco.praisesreminder
 
+import android.app.Application
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
+import jp.co.charco.praisesreminder.data.datastore.SettingDataStore
 import jp.co.charco.praisesreminder.data.db.entity.Praise
 import jp.co.charco.praisesreminder.data.repository.PraiseRepository
+import jp.co.charco.praisesreminder.util.AlarmHelper
 import jp.co.charco.praisesreminder.util.Event
 import jp.co.charco.praisesreminder.util.singleLiveData
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.launch
 import java.time.Instant
@@ -16,11 +21,14 @@ import java.time.ZoneId
 import java.time.ZoneOffset
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
+import java.util.*
 
 @ExperimentalCoroutinesApi
 class MainViewModel @ViewModelInject constructor(
-    private val praiseRepository: PraiseRepository
-) : ViewModel() {
+    application: Application,
+    private val praiseRepository: PraiseRepository,
+    private val settingDataStore: SettingDataStore
+) : AndroidViewModel(application) {
     private val now = LocalDate.now()
     private val currentLocalDateSubject = MutableStateFlow(now)
     val currentEpochMilli: Long
@@ -41,6 +49,27 @@ class MainViewModel @ViewModelInject constructor(
 
     private val _showInputBottomSheet = MutableLiveData<Event<Praise>>()
     val showInputBottomSheet: LiveData<Event<Praise>> = _showInputBottomSheet.distinctUntilChanged()
+
+    init {
+        viewModelScope.launch {
+            // TODO: 設定時刻を可変にしたい
+            settingDataStore.isAlarmInitialized.filter { it.not() }.collect {
+                // 19時にアラームを設定する。nowが19時以降である場合は、次の日からアラームを作動させる
+                val now = Calendar.getInstance()
+                val nextAlarmCalendar = Calendar.getInstance().apply {
+                    set(Calendar.HOUR_OF_DAY, 19)
+                    set(Calendar.MINUTE, 0)
+                }
+
+                if (now >= nextAlarmCalendar) {
+                    nextAlarmCalendar.add(Calendar.DATE, 1)
+                }
+
+                AlarmHelper.createDailyAlarm(getApplication(), 0, nextAlarmCalendar)
+                settingDataStore.updateIsAlarmInitialized(true)
+            }
+        }
+    }
 
     fun changeDate(additionalDate: Int) {
         currentLocalDateSubject.value = now.plusDays(additionalDate.toLong())
